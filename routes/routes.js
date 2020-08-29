326d1a748ccf2ce2ce02e3c12cf232ab1518d990
Fernando Merida <fer.merida94@gmail.com>
2020-08-29 10:32:13 -0600

    routes esb

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,18 @@ const controllers = require('../controllers/controllers')
 // declaración de rutas para las acciones en el controlador
 router.get('/', controllers.index)
 
+router.get('/cliente/pedido', controllers.index)
+router.get('/cliente/verify_res', controllers.index)
+router.get('/cliente/verify_rep', controllers.index)
+
+router.get('/restaurante/pedido', controllers.index)
+router.get('/restaurante/status_pedido', controllers.index)
+router.get('/restaurante/status_ready', controllers.index)
+
+router.get('/repartidor/pedido', controllers.index)
+router.get('/repartidor/status_pedido', controllers.index)
+router.get('/repartidor/status_delivered', controllers.index)
+
+
 // exportar modulo
 module.exports = router
